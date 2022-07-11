08d0607698f81733358ae7e8185dd6f6286e424b
mitchgm <mitchgm@bgsu.edu>
2022-07-10 18:51:52 -0700

    first

---
+++ game.js
@@ -0,0 +1,116 @@
+var question = document.querySelector('#question');
+var choices = Array.from(document.querySelectorAll('.choice-text'));
+var progressText = document.querySelector('#progressText');
+var scoreText = document.querySelector('#score');
+var progressBarFull = document.querySelector('#progressBarFull');
+
+
+
+var currentQuestion = {};
+var acceptingAnswers = true;
+var score = 0;
+var questionCounter = 0;
+var availableQuestions = [];
+
+var questions = [
+    {
+        question: 'What is 2 + 2?',
+        choice1: '2',
+        choice2: '4',
+        choice3: '21',
+        choice4: '17',
+        answer: 2,
+    }, 
+    {
+        question: 'What is 5 + 2?',
+        choice1: '2',
+        choice2: '4',
+        choice3: '21',
+        choice4: '7',
+        answer: 4,
+    }, 
+    {
+        question: 'What is 10 + 2?',
+        choice1: '2',
+        choice2: '4',
+        choice3: '12',
+        choice4: '17',
+        answer: 3,
+    }, 
+    {
+        question: 'What is 12 + 2?',
+        choice1: '2',
+        choice2: '14',
+        choice3: '21',
+        choice4: '17',
+        answer: 2,
+    }
+]
+
+var SCORE_POINTS = 100;
+var MAX_Questions = 4;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestions();
+};
+
+getNewQuestions = () => {
+    if (availableQuestions.length === 0 || questionCounter > MAX_Questions) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+    }
+
+    questionCounter ++;
+    progressText.innerText = `Question ${questionCounter} of ${MAX_Questions}`
+    progressBarFull.style.width = `${(questionCounter/MAX_Questions) * 100}%`
+
+    var questionsIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionsIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach(choice => {
+        var number = choice.dataset ['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        var selectedChoice = e.target;
+        var selectedAnswer = selectedChoice.dataset['number'];
+
+        var classToApply = selectedAnswer == currentQuestion.asnwer ? 'correct':
+        'incorrect';
+
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        };
+
+        selectedChoice.parentElement.classList.add(classToApply);
+
+        setTimout = (() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestions()
+        }, 1000);
+
+    })
+});
+
+
+incrementScore = num => {
+    score +=num;
+    scoreText.innerText = score;
+}
+
+startGame();
