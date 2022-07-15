b9cc69ab20a94bafc75fd975bce8926267430b44
mitchgm <mitchgm@bgsu.edu>
2022-07-14 18:20:14 -0700

    5

---
+++ end.js
@@ -0,0 +1,40 @@
+var username = document.querySelector('#username')
+var saveScoreBtn = document.querySelector('#saveScoreBtn')
+var finalScore = document.querySelector('#finalScore')
+var mostRecentScore = document.querySelector('#mostRecentScore')
+
+
+var highScores = JSON.parse(localStorage.getItem('highScores')) || []
+
+var MAX_HIGH_SCORES = 5
+
+finalScore.innerText = mostRecentScore
+
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value
+})
+
+saveHighScore = i => {
+    i.preventDefault()
+
+    var score = {
+        score: mostRecentScore,
+        name: username.value
+    }
+
+    highScores.push(score)
+    highScores.sort((a,b) => {
+        return b.score - a.score
+
+    })
+
+    highScores.splice(5)
+
+    localStorage.setItem('highScores', JSON.stringify(highScores))
+    window.location.assign('/')
+}
+
+
+
+
+
